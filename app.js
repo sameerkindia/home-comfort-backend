6b6a25449557dc4ecedeca43e99565064f34c056
sameer khan <sameerkhanaa5@gmail.com>
2024-01-16 17:30:17 +0530

    live

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const productRoutes = require("./routes/productRoutes");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.port || 7000;
+const port = process.env.PORT || 7000;
 
 app.use(express.json());
 
